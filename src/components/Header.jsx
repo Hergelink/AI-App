405add6668c9f67008a0f4d4b6182ebb7d81c6f4
Hergelink <enver.hergelink@gmail.com>
2022-11-18 14:56:19 +0300

    added the desktop navbar and adjusted the css for it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import codeIcon from '../images/header-icons/code-icon.png';
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [desktopMenuOpen, setDesktopMenuOpen] = useState(false);
 
   const handleClick = () => {
     setMenuOpen((e) => !e);
@@ -24,6 +25,47 @@ export default function Header() {
             <span></span>
             <span></span>
           </div>
+          <div className='desktopMenu'>
+            <Link to='/' className='homeBtn'>
+              <h3>Home</h3>
+            </Link>
+            <div
+              className='categoryMenu'
+              onMouseEnter={() => setDesktopMenuOpen(true)}
+              onMouseLeave={() => setDesktopMenuOpen(false)}
+            >
+              <h3>Categories</h3>
+
+              {desktopMenuOpen ? (
+                <div className='categoryMenuOpen'>
+                  <ul className='categoryItems'>
+                    <li>
+                      <Link to='product-description'>Product Description</Link>
+                    </li>
+
+                    <li>
+                      <Link to='facebook-ads'>Facebook Ads</Link>
+                    </li>
+
+                    <li>
+                      <Link to='google-ads'>Google Ads</Link>
+                    </li>
+
+                    <li>
+                      <Link to='grammar-correction'>Grammar Correction</Link>
+                    </li>
+
+                    <li>
+                      <Link to='javascript-helper'>JavaScript Helper</Link>
+                    </li>
+                  </ul>
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+            <p className='LoginBtn'>login</p>
+          </div>
         </div>
         {menuOpen ? (
           <div className='mobileModal'>
@@ -36,10 +78,14 @@ export default function Header() {
               </Link>
               <hr />
               <div className='categoryDiv'>
-                <img src={productIcon} className='categoryIcons' alt='product icon' />
+                <img
+                  src={productIcon}
+                  className='categoryIcons'
+                  alt='product icon'
+                />
                 <p>Product</p>
               </div>
-              
+
               <Link to='/product-description' onClick={handleClick}>
                 Product Description
               </Link>
@@ -53,10 +99,14 @@ export default function Header() {
               </Link>
               <hr />
               <div className='categoryDiv'>
-                <img src={languageIcon} className='categoryIcons' alt='language icon' />
+                <img
+                  src={languageIcon}
+                  className='categoryIcons'
+                  alt='language icon'
+                />
                 <p>Language</p>
               </div>
-              
+
               <Link to='/grammar-correction' onClick={handleClick}>
                 Grammar Correction
               </Link>
@@ -65,7 +115,7 @@ export default function Header() {
                 <img src={codeIcon} className='categoryIcons' alt='code icon' />
                 <p>Code</p>
               </div>
-              
+
               <Link to='/javascript-helper' onClick={handleClick}>
                 JavaScript Helper
               </Link>
