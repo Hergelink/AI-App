ed24ee4602a39795156ae46722286e3c14340447
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2022-11-21 16:01:16 +0300

    converted divs to links in the landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,13 @@ import React, { lazy } from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
-import heroImage from '../../images/hero-image.png'
+import heroImage from '../../images/hero-image.png';
+import productIcon from '../../images/header-icons/product-icon.png';
+import languageIcon from '../../images/header-icons/language-icon.png';
+import facebookIcon from '../../images/title-icons/facebook-icon.png';
+import googleIcon from '../../images/title-icons/google-icon.png';
+import javascriptIcon from '../../images/title-icons/javascript-icon.png';
+import youtubeIcon from '../../images/title-icons/youtube-icon.png';
 
 export default function LandingPage() {
   return (
@@ -23,63 +29,62 @@ export default function LandingPage() {
 
         <div className='thirdInfoDiv'>
           <h3>Automatically generate unique and human-like copy in seconds.</h3>
-          <p>Built on top of the world’s most advanced AI language model GPT-3.</p>
+          <p>
+            Built on top of the world’s most advanced AI language model GPT-3.
+          </p>
         </div>
 
         <div className='featuresDiv'>
-          <div className='featureItem'>
-          <div className='featureItemContext'>
-            <h3>Product description generator</h3>
-            <p>Generate product decriptions for your need.</p>
-            </div>
-            <Link to='/product-description' className='checkItOutBtn'>
-              Check it out!
-            </Link>
-          </div>
-
-          <div className='featureItem'>
-          <div className='featureItemContext'>
-            <h3>Facebook Ads generator</h3>
-            <p>Generate Facebook ad copies fast & simple.</p>
-            </div>
-            <Link to='/facebook-ads' className='checkItOutBtn'>
-              Check it out!
-            </Link>
-          </div>
-
-          <div className='featureItem'>
-          <div className='featureItemContext'>
-            <h3>Google Ads generator</h3>
-            <p>Generate Google ad copies fast & simple.</p>
-            </div>
-            <Link to='/product-description' className='checkItOutBtn'>
-              Check it out!
-            </Link>
-          </div>
-
-          <div className='featureItem'>
+          <Link to='/product-description' className='featureItem'>
             <div className='featureItemContext'>
+              <img src={productIcon} alt='product icon' />
+              <h3>Product description generator</h3>
+              <p>Generate product decriptions for your need.</p>
+            </div>
+          </Link>
+
+          <Link to='/facebook-ads' className='featureItem'>
+            <div className='featureItemContext'>
+              <img src={facebookIcon} alt='facebook icon' />
+              <h3>Facebook Ads generator</h3>
+              <p>Generate Facebook ad copies fast & simple.</p>
+            </div>
+          </Link>
+
+          <Link to='/product-description' className='featureItem'>
+            <div className='featureItemContext'>
+              <img src={googleIcon} alt='google icon' />
+              <h3>Google Ads generator</h3>
+              <p>Generate Google ad copies fast & simple.</p>
+            </div>
+          </Link>
+
+          <Link to='/youtube-ideas' className='featureItem'>
+            <div className='featureItemContext'>
+              <img src={youtubeIcon} alt='youtube icon' />
+              <h3>Youtube Idea generator</h3>
+              <p>Generate Youtube titles, video topics and thumbnails. </p>
+            </div>
+          </Link>
+
+          <Link className='featureItem' to='/grammar-correction'>
+            <div className='featureItemContext'>
+              <img src={languageIcon} alt='language icon' />
               <h3>Grammar Correction</h3>
               <p>
                 Correct your sentences with the help of the most powerful
                 language neural network.
               </p>
             </div>
-            <Link to='/grammar-correction' className='checkItOutBtn'>
-              Check it out!
-            </Link>
-          </div>
+          </Link>
 
-          <div className='featureItem'>
+          <Link className='featureItem' to='/javascript-helper'>
             <div className='featureItemContext'>
+              <img src={javascriptIcon} alt='javascript icon' />
               <h3>JavaScript helper</h3>
               <p>Let AI help you with your JavaScript problems.</p>
             </div>
-
-            <Link to='/javascript-helper' className='checkItOutBtn'>
-              Check it out!
-            </Link>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
